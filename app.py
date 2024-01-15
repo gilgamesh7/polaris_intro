c4c9e61d1bf987f04580305b5f4cd93a38b03ccb
Rajesh <45181827+gilgamesh7@users.noreply.github.com>
2024-01-15 06:06:49 +0000

    Add data set & filtering by polars

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import polars as pl
 
 # Print version
 print(f"Polaris version : {pl.__version__}")
+
+# Eager Quickstart
+
+## Import CSV
+df = pl.read_csv("https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv")
+print(f"\n\n{df}\n")
+
+## Filter by column value
+df_filtered_by_sepal_length = df.filter(pl.col("sepal_length") > 5)
+print(f"\n\n{df_filtered_by_sepal_length}\n")
